1023fb52b9758cb86694daa6795d10e0f2829c92
CountNeuroman <olorinoren@gmail.com>
2022-05-28 19:24:53 +0300

    Add ExplicitInterfaceRealizationWithoutBoxing example

---
+++ ExplicitInterfaceRealizationWithoutBoxing/ExplicitInterfaceRealizationWithoutBoxing/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ExplicitInterfaceRealizationWithoutBoxing;
+
+public class Program
+{
+    public static void Main()
+    {
+        var v = new SomeValueType(0);
+        var o = new object();
+        int n = v.CompareTo(v); //Упаковки нет, т.к метод CompareTo принимает SomeValueType
+        //Следующая строка выдает ошибку компиляции, т.к. мы не можем явно скастить object в SomeValueType.
+        //n = v.CompareTo(o);
+    }
+}
+
+internal struct SomeValueType : IComparable
+{
+    private int m_x;
+
+    public SomeValueType(int mX)
+    {
+        m_x = mX;
+    }
+
+    //Реализована безопасность типов, т.к передаем SomeValueType.
+    public int CompareTo(SomeValueType other)
+    {
+        return m_x - other.m_x;
+    }
+
+    //Реализуем контракт Icomparable.
+    int IComparable.CompareTo(object other)
+    {
+        return CompareTo((SomeValueType)other);
+    }
+}
