0532c83a3bf5188ccb76a07a1af3ed42cbc1c394
CountNeuroman <olorinoren@gmail.com>
2022-05-28 13:48:06 +0300

    Add CallInterfaceMethod example

---
+++ CallInterfaceMethod/CallInterfaceMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CallInterfaceMethod;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var simpleType = new SimpleType();
+        
+        simpleType.Dispose();
+
+        IDisposable disposable = simpleType;
+        
+        disposable.Dispose();
+
+        Console.ReadKey();
+    }
+}
+
+internal sealed class SimpleType : IDisposable
+{
+    public void Dispose()
+    {
+        Console.WriteLine("Public Dispose");
+    }
+
+    void IDisposable.Dispose()
+    {
+        Console.WriteLine("IDisposable.Dispose");
+    }
+}
+
