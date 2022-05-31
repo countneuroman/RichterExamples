86f4ec833e31ccd3c8da540c125c786204c3007e
CountNeuroman <olorinoren@gmail.com>
2022-05-31 23:09:27 +0300

    Add GetNumericValue example

---
+++ Char_GetNumericValue/Char_GetNumericValue/Program.cs
@@ -0,0 +1,9 @@
+﻿double d;
+d = char.GetNumericValue('\u0033'); //number 3
+Console.WriteLine(d.ToString());
+
+d = char.GetNumericValue('\u00bc'); // (1/4)
+Console.WriteLine(d.ToString());
+
+d = char.GetNumericValue('A');
+Console.WriteLine(d.ToString());
