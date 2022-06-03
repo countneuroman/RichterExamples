7c4943d9091ab270f0b73984e63b9ac6a0b216e3
CountNeuroman <olorinoren@gmail.com>
2022-06-03 22:51:56 +0300

    Add compare string example

---
+++ StringCompare/StringCompare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace StringCompare;
+
+public class Program
+{
+    public static void Main()
+    {
+        var s = "Hello";
+        var s1 = "Hello";
+
+        //Ordinal быстрее сравнивает строки, т.к не проверяет региональные стандарты.
+        //Полезно использовать для сравнения значений, которые используются внутри приложения и не видны пользователям.
+        int result = string.Compare(s, s1, StringComparison.OrdinalIgnoreCase);
+
+        bool startsWith = s.StartsWith(s1, StringComparison.CurrentCultureIgnoreCase);
+
+        bool equals = s.Equals(s1, StringComparison.CurrentCultureIgnoreCase);
+
+        var ci = new CultureInfo("en-US");
+
+        int compareInfo = ci.CompareInfo.Compare(s, s1);
+
+        var comparer = StringComparer.CurrentCultureIgnoreCase;
+
+        int compare = comparer.Compare(s, s1);
+    }
+}
